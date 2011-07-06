cb2a6861383420823f073d54fee61279d408a2bd
Allen Dang <allengnr@gmail.com>
2011-07-06 23:48:25 +0800

    Add DragAcceptFiles,DragQueryFile,DragQueryPoint ,DragFinish.

--- src/pkg/w32/shell32/shell32.go
+++ src/pkg/w32/shell32/shell32.go
@@ -15,6 +15,10 @@ var (
 
     procSHBrowseForFolder   uintptr
     procSHGetPathFromIDList uintptr
+    procDragAcceptFiles     uintptr
+    procDragQueryFile       uintptr
+    procDragQueryPoint      uintptr
+    procDragFinish          uintptr
 )
 
 func init() {
@@ -22,6 +26,10 @@ func init() {
 
     procSHBrowseForFolder = GetProcAddr(lib, "SHBrowseForFolderW")
     procSHGetPathFromIDList = GetProcAddr(lib, "SHGetPathFromIDListW")
+    procDragAcceptFiles = GetProcAddr(lib, "DragAcceptFiles")
+    procDragQueryFile = GetProcAddr(lib, "DragQueryFileW")
+    procDragQueryPoint = GetProcAddr(lib, "DragQueryPoint")
+    procDragFinish = GetProcAddr(lib, "DragFinish")
 }
 
 func SHBrowseForFolder(bi *BROWSEINFO) uintptr {
@@ -41,4 +49,68 @@ func SHGetPathFromIDList(idl uintptr) string {
         0)
 
     return syscall.UTF16ToString(buf)
+}
+
+func DragAcceptFiles(hwnd HWND, accept bool) {
+    syscall.Syscall(procDragAcceptFiles, 2,
+        uintptr(hwnd),
+        uintptr(BoolToBOOL(accept)),
+        0)
+}
+
+func DragQueryFile(hDrop HDROP, iFile uint) (fileName string, fileCount uint) {
+    ret, _, _ := syscall.Syscall6(procDragQueryFile, 4,
+        uintptr(hDrop),
+        uintptr(iFile),
+        0,
+        0,
+        0,
+        0)
+
+    fileCount = uint(ret)
+
+    if iFile != 0xFFFFFFFF {
+        buf := make([]uint16, fileCount+1)
+
+        ret, _, _ := syscall.Syscall6(procDragQueryFile, 4,
+            uintptr(hDrop),
+            uintptr(iFile),
+            uintptr(unsafe.Pointer(&buf[0])),
+            uintptr(fileCount+1),
+            0,
+            0)
+
+        if ret == 0 {
+            panic("Invoke DragQueryFile error.")
+        }
+
+        fileName = syscall.UTF16ToString(buf)
+    }
+
+    return
+}
+
+func DragQueryPoint(hDrop HDROP) (x, y int, isClientArea bool) {
+    var pt POINT
+    ret, _, _ := syscall.Syscall(procDragQueryPoint, 2,
+        uintptr(hDrop),
+        uintptr(unsafe.Pointer(&pt)),
+        0)
+
+    isClientArea = false
+    if ret == 1 {
+        isClientArea = true
+    }
+
+    x = pt.X
+    y = pt.Y
+
+    return
+}
+
+func DragFinish(hDrop HDROP) {
+    syscall.Syscall(procDragFinish, 1,
+        uintptr(hDrop),
+        0,
+        0)
 }
