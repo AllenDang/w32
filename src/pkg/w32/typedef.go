3eb72eac905371b51dbb86346868cc63e481d85b
Allen Dang <allengnr@gmail.com>
2011-07-10 17:22:27 +0800

    Add typedef and constant support for new gdi32 and user32 methods.

--- src/pkg/w32/typedef.go
+++ src/pkg/w32/typedef.go
@@ -9,22 +9,26 @@ import (
 )
 
 type (
-    BOOL      int
-    ATOM      uint16
-    HANDLE    uintptr
-    HINSTANCE HANDLE
-    HACCEL    HANDLE
-    HCURSOR   HANDLE
-    HDWP      HANDLE
-    HICON     HANDLE
-    HMENU     HANDLE
-    HWND      HANDLE
-    HBRUSH    HANDLE
-    HRESULT   uint32
-    HFONT     HANDLE
-    HDC       HANDLE
-    HGDIOBJ   HANDLE
-    HDROP     HANDLE
+    BOOL         int
+    ATOM         uint16
+    HANDLE       uintptr
+    HINSTANCE    HANDLE
+    HACCEL       HANDLE
+    HCURSOR      HANDLE
+    HDWP         HANDLE
+    HICON        HANDLE
+    HMENU        HANDLE
+    HWND         HANDLE
+    HBRUSH       HANDLE
+    HRESULT      uint32
+    HFONT        HANDLE
+    HDC          HANDLE
+    HGDIOBJ      HANDLE
+    HDROP        HANDLE
+    HENHMETAFILE HANDLE
+    HBITMAP      HANDLE
+    HPEN         HANDLE
+    COLORREF     uint
 )
 
 type POINT struct {
@@ -145,4 +149,130 @@ type EXCEPINFO struct {
     PvReserved        uintptr
     PfnDeferredFillIn uintptr
     Scode             int32
+}
+
+type LOGBRUSH struct {
+    LbStyle uint
+    LbColor COLORREF
+    LbHatch uintptr
+}
+
+type DEVMODE struct {
+    DmDeviceName       [CCHDEVICENAME]uint16
+    DmSpecVersion      uint16
+    DmDriverVersion    uint16
+    DmSize             uint16
+    DmDriverExtra      uint16
+    DmFields           uint
+    DmOrientation      int16
+    DmPaperSize        int16
+    DmPaperLength      int16
+    DmPaperWidth       int16
+    DmScale            int16
+    DmCopies           int16
+    DmDefaultSource    int16
+    DmPrintQuality     int16
+    DmColor            int16
+    DmDuplex           int16
+    DmYResolution      int16
+    DmTTOption         int16
+    DmCollate          int16
+    DmFormName         [CCHFORMNAME]uint16
+    DmLogPixels        uint16
+    DmBitsPerPel       uint
+    DmPelsWidth        uint
+    DmPelsHeight       uint
+    DmDisplayFlags     uint
+    DmDisplayFrequency uint
+    DmICMMethod        uint
+    DmICMIntent        uint
+    DmMediaType        uint
+    DmDitherType       uint
+    DmReserved1        uint
+    DmReserved2        uint
+    DmPanningWidth     uint
+    DmPanningHeight    uint
+}
+
+type BITMAPINFOHEADER struct {
+    BiSize          uint
+    BiWidth         int
+    BiHeight        int
+    BiPlanes        uint16
+    BiBitCount      uint16
+    BiCompression   uint
+    BiSizeImage     uint
+    BiXPelsPerMeter int
+    BiYPelsPerMeter int
+    BiClrUsed       uint
+    BiClrImportant  uint
+}
+
+type RGBQUAD struct {
+    RgbBlue     byte
+    RgbGreen    byte
+    RgbRed      byte
+    RgbReserved byte
+}
+
+type BITMAPINFO struct {
+    BmiHeader BITMAPINFOHEADER
+    BmiColors *RGBQUAD
+}
+
+type ENHMETAHEADER struct {
+    IType          uint
+    NSize          uint
+    RclBounds      RECT
+    RclFrame       RECT
+    DSignature     uint
+    NVersion       uint
+    NBytes         uint
+    NRecords       uint
+    NHandles       uint16
+    SReserved      uint16
+    NDescription   uint
+    OffDescription uint
+    NPalEntries    uint
+    SzlDevice      SIZE
+    SzlMillimeters SIZE
+    CbPixelFormat  uint
+    OffPixelFormat uint
+    BOpenGL        uint
+    SzlMicrometers SIZE
+}
+
+type SIZE struct {
+    CX, CY int
+}
+
+type TEXTMETRIC struct {
+    TmHeight           int
+    TmAscent           int
+    TmDescent          int
+    TmInternalLeading  int
+    TmExternalLeading  int
+    TmAveCharWidth     int
+    TmMaxCharWidth     int
+    TmWeight           int
+    TmOverhang         int
+    TmDigitizedAspectX int
+    TmDigitizedAspectY int
+    TmFirstChar        uint16
+    TmLastChar         uint16
+    TmDefaultChar      uint16
+    TmBreakChar        uint16
+    TmItalic           byte
+    TmUnderlined       byte
+    TmStruckOut        byte
+    TmPitchAndFamily   byte
+    TmCharSet          byte
+}
+
+type DOCINFO struct {
+    CbSize       int32
+    LpszDocName  *uint16
+    LpszOutput   *uint16
+    LpszDatatype *uint16
+    FwType       uint
 }
