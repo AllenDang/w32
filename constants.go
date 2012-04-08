9b948412df7944351fa18f3b7cfae5ec66d50ffe
Allen Dang <allengnr@gmail.com>
2012-04-08 15:28:32 +0800

    Add constants related to clipboard.

--- constants.go
+++ constants.go
@@ -2154,18 +2154,31 @@ const (
 
 // Clipboard formats
 const (
-    CF_TEXT         = 1
-    CF_BITMAP       = 2
-    CF_METAFILEPICT = 3
-    CF_SYLK         = 4
-    CF_DIF          = 5
-    CF_TIFF         = 6
-    CF_OEMTEXT      = 7
-    CF_DIB          = 8
-    CF_PALETTE      = 9
-    CF_PENDATA      = 10
-    CF_RIFF         = 11
-    CF_WAVE         = 12
-    CF_UNICODETEXT  = 13
-    CF_ENHMETAFILE  = 14
+    CF_TEXT            = 1
+    CF_BITMAP          = 2
+    CF_METAFILEPICT    = 3
+    CF_SYLK            = 4
+    CF_DIF             = 5
+    CF_TIFF            = 6
+    CF_OEMTEXT         = 7
+    CF_DIB             = 8
+    CF_PALETTE         = 9
+    CF_PENDATA         = 10
+    CF_RIFF            = 11
+    CF_WAVE            = 12
+    CF_UNICODETEXT     = 13
+    CF_ENHMETAFILE     = 14
+    CF_HDROP           = 15
+    CF_LOCALE          = 16
+    CF_DIBV5           = 17
+    CF_MAX             = 18
+    CF_OWNERDISPLAY    = 0x0080
+    CF_DSPTEXT         = 0x0081
+    CF_DSPBITMAP       = 0x0082
+    CF_DSPMETAFILEPICT = 0x0083
+    CF_DSPENHMETAFILE  = 0x008E
+    CF_PRIVATEFIRST    = 0x0200
+    CF_PRIVATELAST     = 0x02FF
+    CF_GDIOBJFIRST     = 0x0300
+    CF_GDIOBJLAST      = 0x03FF
 )
