4fe0006dbaee1513e0412434a49ef0ab990a6b97
papplampe <papplampe@github.com>
2012-06-05 15:10:09 +0200

    applied 4 spaces format

--- constants.go
+++ constants.go
@@ -2201,8 +2201,8 @@ const (
 
 // MapVirtualKey maptypes
 const (
-	MAPVK_VK_TO_CHAR   = 2
-	MAPVK_VK_TO_VSC    = 0
-	MAPVK_VSC_TO_VK    = 1
-	MAPVK_VSC_TO_VK_EX = 3
+    MAPVK_VK_TO_CHAR   = 2
+    MAPVK_VK_TO_VSC    = 0
+    MAPVK_VSC_TO_VK    = 1
+    MAPVK_VSC_TO_VK_EX = 3
 )
