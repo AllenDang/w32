ea26dc40375bdbd090e57f769941f3af12c25725
papplampe <papplampe@github.com>
2012-06-22 17:06:10 +0200

    added event log functions

--- typedef.go
+++ typedef.go
@@ -413,3 +413,22 @@ type PAINTSTRUCT struct {
     FIncUpdate  BOOL
     RgbReserved [32]byte
 }
+
+type EVENTLOGRECORD struct {
+    Length              uint32
+    Reserved            uint32
+    RecordNumber        uint32
+    TimeGenerated       uint32
+    TimeWritten         uint32
+    EventID             uint32
+    EventType           uint16
+    NumStrings          uint16
+    EventCategory       uint16
+    ReservedFlags       uint16
+    ClosingRecordNumber uint32
+    StringOffset        uint32
+    UserSidLength       uint32
+    UserSidOffset       uint32
+    DataLength          uint32
+    DataOffset          uint32
+}
