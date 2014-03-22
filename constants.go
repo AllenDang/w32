2ce920ca53e2199e1d3063367fe7022cf9d3235c
fromkeith <keith@fromkeith.com>
2014-03-22 10:48:50 -0700

    Fix other spaces v tabs.

--- constants.go
+++ constants.go
@@ -2665,9 +2665,9 @@ const (
 const (
 	GW_HWNDFIRST        = 0
 	GW_HWNDLAST         = 1
-	GW_HWNDNEXT 		= 2
-	GW_HWNDPREV 		= 3
-	GW_OWNER 			= 4
-	GW_CHILD 			= 5
+	GW_HWNDNEXT         = 2
+	GW_HWNDPREV         = 3
+	GW_OWNER            = 4
+	GW_CHILD            = 5
 	GW_ENABLEDPOPUP     = 6
 )
