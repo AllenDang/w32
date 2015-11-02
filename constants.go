d263f1d0630cf29f550d60cf2d827aecd14b6461
Xackery <xackery@gmail.com>
2015-11-02 15:54:58 -0800

    added PROCESS_ALL_ACCESS since it seems to work for writing in windows 10

--- constants.go
+++ constants.go
@@ -1316,18 +1316,19 @@ const (
 //Process Access Rights
 //https://msdn.microsoft.com/en-us/library/windows/desktop/ms684880(v=vs.85).aspx
 const (
-	PROCESS_CREATE_PROCESS            = 0x0080 //Required to create a process.
-	PROCESS_CREATE_THREAD             = 0x0002 //Required to create a thread.
-	PROCESS_DUP_HANDLE                = 0x0040 //Required to duplicate a handle using DuplicateHandle.
-	PROCESS_QUERY_INFORMATION         = 0x0400 //Required to retrieve certain information about a process, such as its token, exit code, and priority class (see OpenProcessToken).
-	PROCESS_QUERY_LIMITED_INFORMATION = 0x1000 //Required to retrieve certain information about a process (see GetExitCodeProcess, GetPriorityClass, IsProcessInJob, QueryFullProcessImageName). A handle that has the PROCESS_QUERY_INFORMATION access right is automatically granted
-	PROCESS_SET_INFORMATION           = 0x0200 //Required to set certain information about a process, such as its priority class (see SetPriorityClass).
-	PROCESS_SET_QUOTA                 = 0x0100 //Required to set memory limits using SetProcessWorkingSetSize.
-	PROCESS_SUSPEND_RESUME            = 0x0800 //Required to suspend or resume a process.
-	PROCESS_TERMINATE                 = 0x0001 //Required to terminate a process using TerminateProcess.
-	PROCESS_VM_OPERATION              = 0x0008 //Required to perform an operation on the address space of a process (see VirtualProtectEx and WriteProcessMemory).
-	PROCESS_VM_READ                   = 0x0010 //Required to read memory in a process using ReadProcessMemory.
-	PROCESS_VM_WRITE                  = 0x0020 //Required to write to memory in a process using WriteProcessMemory.
+	PROCESS_CREATE_PROCESS            = 0x0080  //Required to create a process.
+	PROCESS_CREATE_THREAD             = 0x0002  //Required to create a thread.
+	PROCESS_DUP_HANDLE                = 0x0040  //Required to duplicate a handle using DuplicateHandle.
+	PROCESS_QUERY_INFORMATION         = 0x0400  //Required to retrieve certain information about a process, such as its token, exit code, and priority class (see OpenProcessToken).
+	PROCESS_QUERY_LIMITED_INFORMATION = 0x1000  //Required to retrieve certain information about a process (see GetExitCodeProcess, GetPriorityClass, IsProcessInJob, QueryFullProcessImageName). A handle that has the PROCESS_QUERY_INFORMATION access right is automatically granted
+	PROCESS_SET_INFORMATION           = 0x0200  //Required to set certain information about a process, such as its priority class (see SetPriorityClass).
+	PROCESS_SET_QUOTA                 = 0x0100  //Required to set memory limits using SetProcessWorkingSetSize.
+	PROCESS_SUSPEND_RESUME            = 0x0800  //Required to suspend or resume a process.
+	PROCESS_TERMINATE                 = 0x0001  //Required to terminate a process using TerminateProcess.
+	PROCESS_VM_OPERATION              = 0x0008  //Required to perform an operation on the address space of a process (see VirtualProtectEx and WriteProcessMemory).
+	PROCESS_VM_READ                   = 0x0010  //Required to read memory in a process using ReadProcessMemory.
+	PROCESS_VM_WRITE                  = 0x0020  //Required to write to memory in a process using WriteProcessMemory.
+	PROCESS_ALL_ACCESS                = 2035711 //This is not recommended.
 )
 
 // ListView messages
