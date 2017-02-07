e6c1d45cc24dd25a27ebb508086fc867b6152ca6
Lars <lars.scheme@gmail.com>
2017-02-07 14:21:58 +0100

    Add access rights constants

--- constants.go
+++ constants.go
@@ -2679,3 +2679,22 @@ const (
 	WH_SHELL           = 10
 	WH_SYSMSGFILTER    = 6
 )
+
+// Process Security and Access Rights
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684880(v=vs.85).aspx
+const (
+	PROCESS_TERMINATE                 = 0x0001
+	PROCESS_CREATE_THREAD             = 0x0002
+	PROCESS_SET_SESSIONID             = 0x0004
+	PROCESS_VM_OPERATION              = 0x0008
+	PROCESS_VM_READ                   = 0x0010
+	PROCESS_VM_WRITE                  = 0x0020
+	PROCESS_DUP_HANDLE                = 0x0040
+	PROCESS_CREATE_PROCESS            = 0x0080
+	PROCESS_SET_QUOTA                 = 0x0100
+	PROCESS_SET_INFORMATION           = 0x0200
+	PROCESS_QUERY_INFORMATION         = 0x0400
+	PROCESS_SUSPEND_RESUME            = 0x0800
+	PROCESS_QUERY_LIMITED_INFORMATION = 0x1000
+	PROCESS_SET_LIMITED_INFORMATION   = 0x2000
+)
